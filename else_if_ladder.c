d74cad096e8a2aa42bac01bce6127fee215e64f2
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-07 23:42:01 +0530

    Create else_if_ladder.c

---
+++ else_if_ladder.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+int n;
+printf("Enter a number-->");
+scanf("%d",&n);
+if (n>0)
+printf("Number is Positive");
+else if (n==0)
+printf("Number is zero");
+else
+printf("Number is negative");
+return 0;
+}
